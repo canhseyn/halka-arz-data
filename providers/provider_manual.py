fd743d475a773b45a88c6c84da2a8b3fb5e55694
canhseyn <canhseyn@gmail.com>
2026-07-07 16:46:11 +0300

    Update provider_manual.py

--- providers/provider_manual.py
+++ providers/provider_manual.py
@@ -1,1 +1,14 @@
+from .provider import Provider
+from datetime import datetime
 
+
+class ManualProvider(Provider):
+
+    def get_ipos(self):
+
+        return {
+            "updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "active": [],
+            "upcoming": [],
+            "completed": []
+        }
