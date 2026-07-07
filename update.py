261aa1280c9a1db966d3afbc76581176a61867fa
canhseyn <canhseyn@gmail.com>
2026-07-07 16:46:46 +0300

    Update update.py

--- update.py
+++ update.py
@@ -1,14 +1,12 @@
 import json
-from datetime import datetime
 
-data = {
-    "updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "active": [],
-    "upcoming": [],
-    "completed": []
-}
+from providers.provider_manual import ManualProvider
+
+provider = ManualProvider()
+
+data = provider.get_ipos()
 
 with open("halkaarz.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
 
-print("halkaarz.json güncellendi")
+print("JSON güncellendi")
