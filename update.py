e7813e3a06e68bb948fcdb19e720931e6d501f6c
canhseyn <canhseyn@gmail.com>
2026-07-07 21:28:35 +0300

    Update update.py

--- update.py
+++ update.py
@@ -2,16 +2,16 @@ import json
 from datetime import datetime
 
 from providers.provider_manual import ManualProvider
+from providers.provider_spk import SpkProvider
 
-provider = ManualProvider()
-
-manual = provider.get_ipos()
+manual = ManualProvider().get_ipos()
+spk = SpkProvider().get_ipos()
 
 data = {
     "updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "active": manual.get("active", []),
-    "upcoming": manual.get("upcoming", []),
-    "completed": manual.get("completed", [])
+    "active": spk["active"] + manual["active"],
+    "upcoming": spk["upcoming"] + manual["upcoming"],
+    "completed": spk["completed"] + manual["completed"]
 }
 
 with open("halkaarz.json", "w", encoding="utf-8") as f:
