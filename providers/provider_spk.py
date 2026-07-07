9ee8c8819d2247b5a68a2c547855864d015691b4
canhseyn <canhseyn@gmail.com>
2026-07-07 21:22:14 +0300

    Update provider_spk.py

--- providers/provider_spk.py
+++ providers/provider_spk.py
@@ -1,1 +1,21 @@
 
+import requests
+
+from .provider import Provider
+
+
+class SpkProvider(Provider):
+
+    BASE_URL = ""
+
+    def get_ipos(self):
+        """
+        SPK verilerini döndürecek.
+        Şimdilik boş liste döndürüyoruz.
+        """
+
+        return {
+            "active": [],
+            "upcoming": [],
+            "completed": []
+        }
