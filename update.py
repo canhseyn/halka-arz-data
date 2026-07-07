c381ab6ee7cd1115214a183b983d09244f58c06b
canhseyn <canhseyn@gmail.com>
2026-07-07 22:14:09 +0300

    Update update.py

--- update.py
+++ update.py
@@ -7,14 +7,15 @@ from providers.provider_spk import SpkProvider
 manual = ManualProvider().get_ipos()
 spk = SpkProvider().get_ipos()
 
+
 data = {
     "updated": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
-    "active": spk["active"] + manual["active"],
-    "upcoming": spk["upcoming"] + manual["upcoming"],
+    "active": manual["active"],
+    "upcoming": manual["upcoming"],
     "completed": spk["completed"] + manual["completed"]
 }
 
 with open("halkaarz.json", "w", encoding="utf-8") as f:
     json.dump(data, f, ensure_ascii=False, indent=2)
 
-print("halkaarz.json güncellendi")
+print(f"{len(spk['completed'])} kayıt SPK'dan alındı.")
